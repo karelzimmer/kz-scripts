f14cb031b1f42079cc990faf04832087b910d8bd
Karel Zimmer <karel.zimmer@gmail.com>
2021-08-17 08:50:19 +0200

    Inrichten pc-van-soe

--- kz_install_ubuntu_20.04_desktop_pc-van-soe.sh
+++ kz_install_ubuntu_20.04_desktop_pc-van-soe.sh
@@ -6,8 +6,8 @@
 # Auteursrecht (c) 2021 Karel Zimmer.
 # GNU Algemene Publieke Licentie <https://www.gnu.org/licenses/gpl.html>.
 #
-# ReleaseNumber: 01.00.00
-# DateOfRelease: 2021-08-14
+# ReleaseNumber: 01.01.00
+# DateOfRelease: 2021-08-16
 ###############################################################################
 
 #1 calibre
@@ -15,6 +15,13 @@
 sudo apt-get install --yes calibre
 #4 Start Terminalvenster en voer uit:
 #4    sudo apt remove --yes calibre
+
+
+#1 clamav
+#1 ClamAV installeren
+sudo apt-get install --yes clamtk-gnome
+#4 Start Terminalvenster en voer uit:
+#4    sudo apt remove clamtk-gnome
 
 
 # EOF
