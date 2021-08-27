37b188722f7c75951dfc48c2f9392ecc7a3de8be
Karel Zimmer <karel.zimmer@gmail.com>
2021-08-27 15:18:17 +0200

    Hugo VBox VM

--- kz_vm_hugowin732.sh
+++ kz_vm_hugowin732.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 # RELEASE_YEAR=2017
 
-# VERSION_NUMBER=01.00.00
-# VERSION_DATE=2017-07-28
+# VERSION_NUMBER=02.00.00
+# VERSION_DATE=2021-08-27
 
-sleep 10
-VBoxManage startvm "hugowin732"
+TITLE='VirtualBox'
+TEXT='Virtuele Machine hugowin732 wordt gestart (kan even duren...)'
+
+start_vm() {
+     while ! lsmod | grep vbox; do
+         sleep 5
+     done
+    VBoxManage startvm "hugowin732"
+}
+
+start_vm |
+zenity  --progress                  \
+             --pulsate              \
+             --auto-close           \
+             --no-cancel            \
+             --width     500        \
+             --height    50         \
+             --title     "$TITLE"   \
+             --text      "$TEXT"
 
 # EOF
