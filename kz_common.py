8f193cd9184388c5f12766d9f35aff242499c185
Karel Zimmer <karel.zimmer@gmail.com>
2021-09-21 10:41:29 +0200

    Verwerk algemene opties naar module kz_common

--- kz_common.py
+++ kz_common.py
@@ -8,16 +8,48 @@ Deze module geeft toegang tot algemene functies.
 #
 # Geschreven door Karel Zimmer <info@karelzimmer.nl>.
 ###############################################################################
+import sys
+import argparse
 import datetime
 
 module_name = 'kz_common.py'
 release_year = 2021
 
-version_number = '02.01.01'
-version_date = '2021-09-08'
+version_number = '02.02.00'
+version_date = '2021-09-21'
+
+
+def process_common_options(desc, display_name):
+    """
+    Deze functie verwerkt de algemene opties.
+    """
+    parser = argparse.ArgumentParser(prog=display_name,
+                                     description=desc,
+                                     epilog="Typ 'man " + display_name +
+                                     "' voor meer informatie.")
+    parser.add_argument('-u', '--usage', action='store_true',
+                        help='een korte gebruikssamenvatting tonen')
+    parser.add_argument('-v', '--version', action='store_true',
+                        help='de versie tonen')
+    parser.add_argument('-g', '--gui', action='store_true',
+                        help='start in grafische modus')
+    args = parser.parse_args()
+
+    if args.usage:
+        process_option_usage(display_name)
+        sys.exit(0)
+    elif args.version:
+        process_option_version(display_name, version_number,
+                               version_date, release_year)
+        sys.exit(0)
+    elif args.gui:
+        return True
 
 
 def process_option_usage(display_name):
+    """
+    Deze functie toont de beschikbare opties.
+    """
     print(f"""Gebruik: {display_name} [-u|--usage] [-h|--help] [-v|--version] \
 [-g|--gui]
 
