9b8706fb3201e0f80a543dfd4cf9442fd01f1d7b
Karel Zimmer <karel.zimmer@gmail.com>
2021-11-30 10:49:00 +0100

    Remove named temp files

--- kz-common.sh
+++ kz-common.sh
@@ -440,7 +440,7 @@ dan een Terminalvenster, en voer uit:
             ;;
         kz-install)
             printf '%s' "${NORMAL}${CURSOR_VISABLE}"
-            rm $CMDS_FILE $CMDS_FILE2 $TEXT_FILE $TEMP_LIST
+            rm "$CMDS_FILE" "$CMDS_FILE2" "$TEXT_FILE" "$TEMP_LIST"
             if [[ $rc -ne $SUCCESS ]]; then
                 log "Als de pakketbeheerder 'apt' foutmeldingen geeft, start \
 dan een Terminalvenster, en voer uit:
@@ -452,7 +452,7 @@ dan een Terminalvenster, en voer uit:
             ;;
         kz-setup)
             printf '%s' "${NORMAL}${CURSOR_VISABLE}"
-            rm $CMDS_FILE $CMDS_FILE2 $TEXT_FILE
+            rm "$CMDS_FILE" "$CMDS_FILE2" "$TEXT_FILE"
             ;;
         *)
             return $SUCCESS
