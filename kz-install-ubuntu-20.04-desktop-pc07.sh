aa29ff298aa0a87c735c6cb9d74f91685d0385f1
Karel Zimmer <karel.zimmer@gmail.com>
2022-01-13 09:18:33 +0100

    Install/setup pc07 back from archive

--- kz-install-ubuntu-20.04-desktop-pc07.sh
+++ kz-install-ubuntu-20.04-desktop-pc07.sh
@@ -1,51 +1,15 @@
 # shellcheck shell=bash
 ###############################################################################
-# Installatiebestand voor Ubuntu20.04 LST desktop op pc07.
-#
-# Geschreven door Karel Zimmer <info@karelzimmer.nl>.
+# Installatiebestand voor Ubuntu20.04 LST desktop op pc07.                    #
+#                                                                             #
+# Geschreven door Karel Zimmer <info@karelzimmer.nl>.                         #
 ###############################################################################
-# RELEASE_YEAR=2020
-
-# VERSION_NUMBER=03.02.00
-# VERSION_DATE=2021-09-03
-
-
-#1 bluefish
-#2 Bluefish (editor) installeren
-sudo apt-get install --yes bluefish
-#4 Start Terminalvenster en voer uit:
-#4    sudo apt remove --yes bluefish
-
-
-#1 gast
-#2 Gastgebruiker (gast) installeren
-sudo useradd --create-home --shell /bin/bash --comment 'Gast' gast || true
-sudo chmod 0750 /home/gast
-sudo passwd --delete gast
-#4 Start Terminalvenster en voer uit:
-#4    sudo userdel --remove gast
-
-
-#1 exiftool
-#2 ExifTool (metadata lezen en schrijven) installeren
-sudo apt-get install --yes libimage-exiftool-perl
-#4 Start Terminalvenster en voer uit:
-#4    sudo apt remove --yes libimage-exiftool-perl
-
-
-#1 gedit
-#2 Gedit (editor) installeren
-sudo apt-get install --yes gedit-plugins
-#4 Start Terminalvenster en voer uit:
-#4    sudo apt remove --yes gedit-plugins
-
 
 #1 git
 #2 Git (versiebeheersysteem) installeren
 sudo apt-get install --yes git git-gui qgit
 #4 Start Terminalvenster en voer uit:
 #4    sudo apt remove --yes git git-gui qgit
-
 
 #1 kvm
 #2 KVM (virtualisatie) installeren
@@ -62,20 +26,17 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes --option Dpkg::Options
 #4    sudo deluser ${SUDO_USER:-$USER} libvirtd
 #4    sudo delgroup libvirtd
 
-
 #1 locate
 #2 Locate  (bestanden snel zoeken op naam) installeren
 sudo apt-get install --yes mlocate
 #4 Start Terminalvenster en voer uit:
 #4    sudo apt remove --yes mlocate
 
-
 #1 meld
 #2 Meld (visuele diff en merge tool) installeren
 sudo apt-get install --yes meld
 #4 Start Terminalvenster en voer uit:
 #4    sudo apt remove --yes meld
-
 
 #1 python
 #2 Python (programmeertaal) installeren
@@ -84,7 +45,6 @@ sudo ln --force --relative --symbolic /usr/bin/pip3 /usr/bin/pip
 #4 Start Terminalvenster en voer uit:
 #4    sudo apt remove --yes idle python3-pycodestyle python3-autopep8 python3-pip python-is-python3
 #4    sudo rm --force /usr/bin/pip
-
 
 #1 signal
 #2 Signal (privéberichtenapp) installeren
@@ -96,7 +56,6 @@ sudo apt-get install --yes signal-desktop
 #4    sudo apt remove --yes signal-desktop
 #4    sudo rm /etc/apt/sources.list.d/signal-xenial.list
 #4    sudo apt-get update
-
 
 #1 tlp
 #2 TLP (accubesparing) installeren
@@ -113,19 +72,8 @@ sudo apt-get install --yes tp-smapi-dkms acpi-call-dkms
 #4 Start Terminalvenster en voer uit:
 #4    sudo apt remove --yes tlp tp-smapi-dkms acpi-call-dkms
 
-
-#1 tree
-#2 Tree (mappenboom weergeven) installeren
-sudo apt-get install --yes tree
-#4 Start Terminalvenster en voer uit:
-#4    sudo apt remove --yes tree
-
-
 #1 vscode
 #2 Visual Studio Code (editor) installeren
 sudo snap install --classic code
 #4 Start Terminalvenster en voer uit:
 #4    sudo snap remove code
-
-
-# EOF
