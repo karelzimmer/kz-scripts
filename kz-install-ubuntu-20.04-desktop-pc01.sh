838f80e7fd0858d24681a76e5492afde35e458be
Karel Zimmer <karel.zimmer@gmail.com>
2022-01-14 13:20:26 +0100

    User karel and ssh deleted

--- kz-install-ubuntu-20.04-desktop-pc01.sh
+++ kz-install-ubuntu-20.04-desktop-pc01.sh
@@ -23,26 +23,6 @@ sudo apt-get install --yes aspell-nl git gitg
 #4 Start Terminalvenster en voer uit:
 #4    sudo apt remove --yes git gitg
 
-#1 karel
-#2 Karel - Systeemgebruiker
-## Voor toegang op afstand en systeembeheer.
-sudo useradd --create-home --shell=/usr/bin/bash --comment='Karel Zimmer' --groups=sudo karel || true
-sudo cp /var/lib/AccountsService/users/{gdm,karel}
-#3 Start Terminalvenster en voer uit:
-#3    sudo passwd karel
-#4 Start Terminalvenster en voer uit:
-#4    sudo userdel --remove karel
-
-#1 openssh
-#2 OpenSSH - Toegang op afstand
-sudo apt-get install --yes ssh
-sudo sed --in-place --expression='s/PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
-## Check op remote root-toegang.
-grep --quiet --regexp='PermitRootLogin no' /etc/ssh/sshd_config
-sudo systemctl restart ssh.service
-#4 Start Terminalvenster en voer uit:
-#4    sudo apt remove --yes ssh
-
 #1 python
 #2 Python - Programmeertaal
 sudo apt-get install --yes pycodestyle python3-pycodestyle python3-autopep8 python3-pip python-is-python3
