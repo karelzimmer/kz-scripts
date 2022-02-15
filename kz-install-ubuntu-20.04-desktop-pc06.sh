2c90d908c5475892b39b7cedc489251a64ed9a0f
Karel Zimmer <karel.zimmer@gmail.com>
2022-02-15 12:40:54 +0100

    Debian to archive

--- kz-install-ubuntu-20.04-desktop-pc06.sh
+++ kz-install-ubuntu-20.04-desktop-pc06.sh
@@ -5,17 +5,17 @@
 # Geschreven door Karel Zimmer <info@karelzimmer.nl>.                         #
 ###############################################################################
 
-#1 calibre (E-boekmanager)
+#1 calibre (e-boekmanager)
 sudo apt-get install --yes calibre
 #3 Start Terminalvenster en voer uit:
 #3    sudo apt remove --yes calibre
 
-#1 git (Versiebeheersysteem)
+#1 git (versiebeheersysteem)
 sudo apt-get install --yes aspell-nl git
 #3 Start Terminalvenster en voer uit:
 #3    sudo apt remove --yes git
 
-#1 kvm (Virtualisatie)
+#1 kvm (virtualisatie)
 ## Images staan in /var/lib/libvirt/images/.
 ## Dpkg::Options i.v.m. interactie a.g.v. restore /etc/libvirt configuratiebestanden.
 sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes --option Dpkg::Options::="--force-confdef" --option Dpkg::Options::="--force-confold" cpu-checker qemu-kvm bridge-utils virt-manager
@@ -34,22 +34,22 @@ sudo apt-get install --yes lftp
 #3 Start Terminalvenster en voer uit:
 #3    sudo apt remove --yes lftp
 
-#1 locate (Bestanden snel zoeken op naam)
+#1 locate (bestanden snel zoeken op naam)
 sudo apt-get install --yes mlocate
 #3 Start Terminalvenster en voer uit:
 #3    sudo apt remove --yes mlocate
 
-#1 meld (Visuele diff en merge tool)
+#1 meld (visuele diff en merge tool)
 sudo apt-get install --yes meld
 #3 Start Terminalvenster en voer uit:
 #3    sudo apt remove --yes meld
 
-#1 pinta (Tekenprogramma)
+#1 pinta (tekenprogramma)
 sudo apt-get install --yes pinta
 #3 Start Terminalvenster en voer uit:
 #3    sudo apt remove --yes pinta
 
-#1 python (Programmeertaal)
+#1 python (programmeertaal)
 sudo apt-get install --yes pycodestyle python3-pycodestyle python3-autopep8 python3-pip python-is-python3
 sudo ln --force --relative --symbolic /usr/bin/pycodestyle /usr/bin/pep8
 sudo ln --force --relative --symbolic /usr/bin/pip3 /usr/bin/pip
@@ -57,12 +57,12 @@ sudo ln --force --relative --symbolic /usr/bin/pip3 /usr/bin/pip
 #3    sudo apt remove --yes pycodestyle python3-pycodestyle python3-autopep8 python3-pip python-is-python3
 #3    sudo rm --force /usr/bin/pip
 
-#1 samba (Communiceren met andere machines)
+#1 samba (communiceren met andere machines)
 sudo apt-get install --yes samba cifs-utils
 #3 Start Terminalvenster en voer uit:
 #3    sudo apt remove --yes samba cifs-utils
 
-#1 signal (Privéberichtenapp)
+#1 signal (privéberichtenapp)
 wget --no-verbose --output-document=- 'https://updates.signal.org/desktop/apt/keys.asc' | sudo apt-key add -
 printf '%s\n' "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee /etc/apt/sources.list.d/signal-xenial.list
 sudo apt-get update
@@ -72,7 +72,7 @@ sudo apt-get install --yes signal-desktop
 #3    sudo rm /etc/apt/sources.list.d/signal-xenial.list
 #3    sudo apt-get update
 
-#1 tree (Mappenboom weergeven)
+#1 tree (mappenboom weergeven)
 sudo apt-get install --yes tree
 #3 Start Terminalvenster en voer uit:
 #3    sudo apt remove --yes tree
