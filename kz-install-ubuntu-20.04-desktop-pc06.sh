334fb16a9b1f8f64a3ca680f85c6a12881a204d3
Karel Zimmer <karel.zimmer@gmail.com>
2022-04-08 14:44:07 +0200

    Reduced number of apps

--- kz-install-ubuntu-20.04-desktop-pc06.sh
+++ kz-install-ubuntu-20.04-desktop-pc06.sh
@@ -21,20 +21,6 @@ sudo apt-get install --yes aspell-nl git
 #3 Start Terminalvenster en voer uit:
 #3    sudo apt remove git
 
-#1 google-earth (verken de wereld)
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/google-earth.gpg] http://dl.google.com/linux/earth/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-earth-pro.list
-wget --no-verbose --output-document=- https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor --yes --output=/usr/share/keyrings/google-earth.gpg
-sudo apt-get update
-sudo apt-get install --yes google-earth-pro-stable
-sudo apt-key del 7FAC5991 D38B4796
-sudo rm --force /etc/apt/trusted.gpg.d/google-earth*
-## Extra needed after first install.
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/google-earth.gpg] http://dl.google.com/linux/earth/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-earth-pro.list
-#3 Start Terminalvenster en voer uit:
-#3    sudo apt remove --yes google-earth-pro-stable
-#3    sudo rm /etc/apt/sources.list.d/google-earth.list* /usr/share/keyrings/google-earth.gpg*
-#3    sudo apt update
-
 #1 kvm (virtualisatie)
 ## Images staan in /var/lib/libvirt/images/.
 ## Dpkg::Options i.v.m. interactie a.g.v. restore /etc/libvirt configuratiebestanden.
