191c6cdc6c058499be2963700e845a398dc61328
Karel Zimmer <karel.zimmer@gmail.com>
2022-04-26 08:21:08 +0200

    Added Ubuntu 22.04

--- kz-setup-ubuntu-22.04-desktop-pc-van-ella.sh
+++ kz-setup-ubuntu-22.04-desktop-pc-van-ella.sh
@@ -5,6 +5,35 @@
 # Geschreven door Karel Zimmer <info@karelzimmer.nl>.                         #
 ###############################################################################
 
+#1 bitwarden (wachtwoordbeheer)
+kz-gset --delfav=bitwarden_bitwarden
+#2 kz-gset --addfavaft=bitwarden_bitwarden
+
+#1 firefox (webbrowser)
+## Zou er al moeten zijn, voor de zekerheid toegevoegd.
+kz-gset --addfavbef=firefox_firefox
+#2 kz-gset --delfav=firefox_firefox
+
 #1 gnome (bureaubladomgeving)
 gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/Fleurs_de_Prunus_24_by_Jérôme_Boivin.jpg
 #2 gsettings reset org.gnome.desktop.background picture-uri
+
+#1 google-chrome (webbrowser)
+kz-gset --delfav=google-chrome
+#2 kz-gset --addfavbef=google-chrome
+
+#1 skype (beeldbellen)
+kz-gset --delfav=skype_skypeforlinux
+#2 kz-gset --addfavaft=skype_skypeforlinux
+
+#1 spotify (muziekspeler)
+kz-gset --delfav=spotify_spotify
+#2 kz-gset --addfavaft=spotify_spotify
+
+#1 teams (samenwerken)
+kz-gset --delfav=teams
+#2 kz-gset --addfavaft=teams
+
+#1 zoom (samenwerken)
+kz-gset --delfav=Zoom
+#2 kz-gset --addfavaft=Zoom
