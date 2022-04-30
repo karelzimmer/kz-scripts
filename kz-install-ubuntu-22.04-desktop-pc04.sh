d32538e391c437aab1aae8fbea480534f2ce8fab
Karel Zimmer <karel.zimmer@gmail.com>
2022-04-30 18:35:08 +0200

    Fixed issue with apt google-earth-pro.list

--- kz-install-ubuntu-22.04-desktop-pc04.sh
+++ kz-install-ubuntu-22.04-desktop-pc04.sh
@@ -14,16 +14,16 @@ sudo apt-get install --yes gnome-gmail
 #2 sudo apt remove --yes gnome-gmail
 
 #1 google-earth (verken de wereld)
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/google-earth.gpg] http://dl.google.com/linux/earth/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-earth.list
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/google-earth.gpg] http://dl.google.com/linux/earth/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-earth-pro.list
 wget --no-verbose --output-document=- https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor --yes --output=/usr/share/keyrings/google-earth.gpg
 sudo apt-get update
 sudo apt-get install --yes google-earth-pro-stable
 sudo apt-key del 7FAC5991 D38B4796
 sudo rm --force /etc/apt/trusted.gpg.d/google-earth*
 ## Extra needed after first install.
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/google-earth.gpg] http://dl.google.com/linux/earth/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-earth.list
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/google-earth.gpg] http://dl.google.com/linux/earth/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-earth-pro.list
 #2 sudo apt remove --yes google-earth-pro-stable
-#2 sudo rm /etc/apt/sources.list.d/google-earth.list* /usr/share/keyrings/google-earth.gpg*
+#2 sudo rm /etc/apt/sources.list.d/google-earth-pro.list* /usr/share/keyrings/google-earth.gpg*
 #2 sudo apt update
 
 #1 wine (windowsapps op Linux)
