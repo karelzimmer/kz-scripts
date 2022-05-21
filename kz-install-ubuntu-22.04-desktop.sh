6705bc8c4e510ed121fdc9f8c88966857b9626bf
Karel Zimmer <karel.zimmer@gmail.com>
2022-05-21 10:34:39 +0200

    Added citrix (telewerken)

--- kz-install-ubuntu-22.04-desktop.sh
+++ kz-install-ubuntu-22.04-desktop.sh
@@ -19,7 +19,7 @@ sudo snap install bitwarden
 
 #1 citrix (telewerken)
 ## Aka Citrix Workspace app, Citrix Receiver, ICA Client.
-wget --no-verbose --output-document=/tmp/icaclient.deb "https://karelzimmer.nl/downloads/icaclient/icaclient_20.04.0.21_amd64.deb"
+wget --no-verbose --output-document=/tmp/icaclient.deb "https://karelzimmer.nl/downloads/citrix/icaclient_20.04.0.21_amd64.deb"
 sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes /tmp/icaclient.deb
 sudo ln --symbolic --force /usr/share/ca-certificates/mozilla/* /opt/Citrix/ICAClient/keystore/cacerts
 sudo c_rehash /opt/Citrix/ICAClient/keystore/cacerts
