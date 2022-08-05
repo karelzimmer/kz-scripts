d6a70cb01dcb4cb660a745cad82b71385c294869
Karel Zimmer <karel.zimmer@gmail.com>
2022-08-05 08:05:33 +0200

    Refactoring

--- kz-setup-ubuntu-22.04-desktop.sh
+++ kz-setup-ubuntu-22.04-desktop.sh
@@ -39,7 +39,7 @@ kz-gset --addfavaft=Zoom
 
 #1 gnome (bureaubladomgeving)
 kz-gset --addappfolder='KZ Scripts'
-if [[ -e $HOME/kz-data/Achtergrond ]]; then gsettings set org.gnome.desktop.background picture-uri "file://$HOME/kz-data/Achtergrond"; fi
+if [[ -f $HOME/kz-data/Achtergrond ]]; then gsettings set org.gnome.desktop.background picture-uri "file://$HOME/kz-data/Achtergrond"; fi
 gsettings set org.gnome.desktop.calendar show-weekdate true
 gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface clock-show-weekday true
@@ -55,7 +55,7 @@ gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
 gsettings set org.gnome.settings-daemon.plugins.power power-button-action 'interactive'
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
-if [[ -e $HOME/kz-data/Favorieten ]]; then gsettings set org.gnome.shell favorite-apps "$(cat "$HOME"/kz-data/Favorieten)"; fi
+if [[ -f $HOME/kz-data/Favorieten ]]; then gsettings set org.gnome.shell favorite-apps "$(cat "$HOME"/kz-data/Favorieten)"; fi
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews'
 gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts-network false
 gsettings set org.gnome.shell.extensions.ding show-home false
