b8a7f009fdcb312863d03fdf3a3f95659648055a
Karel Zimmer <karel.zimmer@gmail.com>
2022-09-09 10:28:42 +0200

    Install developer tools with kz-getdeb

--- kz-install-debian-11-desktop-pc07.sh
+++ kz-install-debian-11-desktop-pc07.sh
@@ -12,6 +12,10 @@ sudo sed --in-place --expression='s/^iface eth0 net dhcp$/#iface eth0 net dhcp/'
 #2 sudo sed --in-place --expression='s/^#auto eth0$/auto eth0/' /etc/network/interfaces.d/setup
 #2 sudo sed --in-place --expression='s/^#iface eth0 net dhcp$/iface eth0 net dhcp/' /etc/network/interfaces.d/setup
 
+#1 git (versiebeheersysteem)
+sudo apt-get install --yes git
+#2 sudo apt-get remove --yes git
+
 #1 spice-vdagent (klembord delen tussen gastheer en gast)
 sudo apt-get install --yes spice-vdagent
 #2 sudo apt-get remove --yes spice-vdagent
@@ -39,6 +43,13 @@ sudo apt-get install --yes mlocate
 #1 pinfo (gebruiksvriendelijke viewer voor Info-documenten)
 sudo apt-get install --yes pinfo
 #2 sudo apt-get remove --yes pinfo
+
+#1 python (programmeertaal)
+sudo apt-get install --yes idle pycodestyle python3-pycodestyle python3-autopep8 python3-pip
+sudo ln --force --relative --symbolic /usr/bin/pip3 /usr/bin/pip
+sudo ln --force --relative --symbolic /usr/bin/python3 /usr/bin/python
+#2 sudo apt-get remove --yes idle pycodestyle python3-pycodestyle python3-autopep8 python3-pip
+#2 sudo rm /usr/bin/pip /usr/bin/python
 
 #1 signal (privéberichtenapp)
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal.gpg] https://updates.signal.org/desktop/apt xenial main' | sudo tee /etc/apt/sources.list.d/signal.list
--- kz-install-ubuntu-22.04-desktop-pc01.sh
+++ kz-install-ubuntu-22.04-desktop-pc01.sh
@@ -22,6 +22,10 @@ sudo useradd --create-home --shell /bin/bash --comment 'Gast' gast || true
 sudo passwd --delete gast
 #2 sudo userdel --remove gast
 
+#1 git (versiebeheersysteem)
+sudo apt-get install --yes git
+#2 sudo apt-get remove --yes git
+
 #1 gnome-gmail (Gmail als e-mailtoepassing in GNOME)
 sudo apt-get install --yes gnome-gmail
 #2 sudo apt-get remove --yes gnome-gmail
@@ -29,6 +33,13 @@ sudo apt-get install --yes gnome-gmail
 #1 pinfo (gebruiksvriendelijke viewer voor Info-documenten)
 sudo apt-get install --yes pinfo
 #2 sudo apt-get remove --yes pinfo
+
+#1 python (programmeertaal)
+sudo apt-get install --yes idle pycodestyle python3-pycodestyle python3-autopep8 python3-pip
+sudo ln --force --relative --symbolic /usr/bin/pip3 /usr/bin/pip
+sudo ln --force --relative --symbolic /usr/bin/python3 /usr/bin/python
+#2 sudo apt-get remove --yes idle pycodestyle python3-pycodestyle python3-autopep8 python3-pip
+#2 sudo rm /usr/bin/pip /usr/bin/python
 
 #1 ssh (veilige shell-client en server)
 sudo apt-get install --yes ssh
