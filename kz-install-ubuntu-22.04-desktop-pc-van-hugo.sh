891b5c08cb8013c2b69bc335d434893fecd57d36
Karel Zimmer <karel.zimmer@gmail.com>
2022-09-20 09:17:40 +0200

    Added youtube downloader

--- kz-install-ubuntu-22.04-desktop-pc-van-hugo.sh
+++ kz-install-ubuntu-22.04-desktop-pc-van-hugo.sh
@@ -79,3 +79,7 @@ sudo rm /tmp/Oracle_VM_VirtualBox_Extension_Pack /tmp/virtualbox-LATEST.TXT
 #2 sudo apt-get remove --yes virtualbox-*
 #2 sudo rm --force /etc/apt/sources.list.d/virtualbox.list* /usr/share/keyrings/oracle_vbox_2016*
 #2 sudo apt-get update
+
+#1 youtube-downloader (download videos)
+sudo apt-get install --yes youtubedl-gui
+#2 sudo apt-get remove --yes youtubedl-gui
