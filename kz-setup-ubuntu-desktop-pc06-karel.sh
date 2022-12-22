7ec0f0b1e69c8ca9501faafe13801d0012ad8848
Karel Zimmer <karel.zimmer@gmail.com>
2022-12-22 11:22:16 +0100

    Remove alias for pinfo

--- kz-setup-ubuntu-desktop-pc06-karel.sh
+++ kz-setup-ubuntu-desktop-pc06-karel.sh
@@ -31,12 +31,8 @@ kz-gset --addfavbef=org.gnome.Terminal
 ## Vooruit zoeken in history met Ctrl-S).
 sed --in-place --expression='/^stty -ixon/d' "$HOME"/.bashrc
 echo 'stty -ixon  # Enable fwd search history (i-search)' >> "$HOME"/.bashrc
-## Gebruiksvriendelijke viewer voor Info-documenten (en man-pagina's).
-sed --in-place --expression='/^alias info=/d' "$HOME"/.bashrc
-echo 'alias info=pinfo # Gebruiksvriendelijke viewer voor Info-documenten' >> "$HOME"/.bashrc
 #2 kz-gset --delfav=org.gnome.Terminal
 #2 sed --in-place --expression='/^stty -ixon/d' "$HOME"/.bashrc
-#2 sed --in-place --expression='/^alias info=/d' "$HOME"/.bashrc
 
 #1 vscode (editor)
 kz-gset --addfavbef=code_code
