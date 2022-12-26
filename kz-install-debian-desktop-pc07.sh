c1647046fe06216d4df529b6dc2b2ef0f7ac5f4d
Karel Zimmer <karel.zimmer@gmail.com>
2022-12-26 11:52:13 +0100

    Add user to group adm

--- kz-install-debian-desktop-pc07.sh
+++ kz-install-debian-desktop-pc07.sh
@@ -6,16 +6,16 @@
 # Publiek Domein Verklaring <http://creativecommons.org/publicdomain/zero/1.0>.
 ###############################################################################
 
+#1-adm (gebruiker toevoegen aan groep adm)
+sudo adduser "${SUDO_USER:-$USER}" adm
+#2 sudo deluser ${SUDO_USER:-$USER} adm
+
 #1-deact-eth0 (activeren eth0 uit start-up halen)
 ## ifup -a --read-environement: Cannot find device "eth0"/ [FAILED] Raise network interfaces
 sudo sed --in-place --expression='s/^auto eth0$/#auto eth0/' /etc/network/interfaces.d/setup
 sudo sed --in-place --expression='s/^iface eth0 net dhcp$/#iface eth0 net dhcp/' /etc/network/interfaces.d/setup
 #2 sudo sed --in-place --expression='s/^#auto eth0$/auto eth0/' /etc/network/interfaces.d/setup
 #2 sudo sed --in-place --expression='s/^#iface eth0 net dhcp$/iface eth0 net dhcp/' /etc/network/interfaces.d/setup
-
-#1 adm (gebruiker toevoegen aan groep adm)
-sudo adduser "${SUDO_USER:-$USER}" adm
-#2 sudo deluser ${SUDO_USER:-$USER} adm
 
 #1 spice-vdagent (klembord delen tussen gastheer en gast)
 sudo apt-get install --yes spice-vdagent
