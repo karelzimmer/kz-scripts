29655851bfc89472b40a21387c2d67610c668d07
Karel Zimmer <karel.zimmer@gmail.com>
2022-12-31 10:49:48 +0100

    Refactoring

--- kz_common.py
+++ kz_common.py
@@ -150,7 +150,7 @@ def process_options(program_name, program_desc, display_name):
     parser.add_argument('-u', '--usage', action='store_true')
     parser.add_argument('-v', '--version', action='store_true')
     args = parser.parse_args()
-    
+
     if args.help:
         process_option_help(display_name, program_desc)
         sys.exit(ok)
@@ -160,6 +160,7 @@ def process_options(program_name, program_desc, display_name):
     elif args.version:
         process_option_version(program_name)
         sys.exit(ok)
+
 
 def process_option_help(display_name, program_desc):
     """
