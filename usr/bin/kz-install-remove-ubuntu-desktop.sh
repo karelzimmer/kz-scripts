3347aca2c8fa07d956855292d3891e42bbf42f25
Karel Zimmer <karel.zimmer@gmail.com>
2023-06-26 14:12:13 +0200

    Remove DESC specification and add comment where needed

--- usr/bin/kz-install-remove-ubuntu-desktop.sh
+++ usr/bin/kz-install-remove-ubuntu-desktop.sh
@@ -181,6 +181,8 @@ sudo snap remove code
 sudo apt-get remove --yes wine winetricks playonlinux
 
 #  APP x11
+# Force to use wayland. With wayland issues with video playback and TeamViewer.
+# To check execute: echo $XDG_SESSION_TYPE (should output 'wayland')
 # HOST *
 sudo sed --in-place --expression='s/WaylandEnable=false/#WaylandEnable=false/' /etc/gdm3/custom.conf
 
