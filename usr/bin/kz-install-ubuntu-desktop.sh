93eda17f798252d15f80ffe832632fdeefe63688
Karel Zimmer <karel.zimmer@gmail.com>
2024-04-29 07:51:26 +0200

    Ubuntu 24.04

--- usr/bin/kz-install-ubuntu-desktop.sh
+++ usr/bin/kz-install-ubuntu-desktop.sh
@@ -280,10 +280,10 @@ sudo delgroup libvirtd
 
 
 # Install APP libreoffice HOST *
-sudo apt-get install --yes aspell-en aspell-nl libreoffice
+sudo apt-get install --yes aspell-en aspell-nl libreoffice libreoffice-l10n-nl
 
 # Remove APP libreoffice HOST *
-sudo apt-get remove --yes aspell-en aspell-nl libreoffice
+sudo apt-get remove --yes aspell-en aspell-nl libreoffice libreoffice-l10n-nl
 
 
 # Install APP locate HOST pc06
