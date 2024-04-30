6bff71e1d6eac97aa5eaf60426d0edda5aa843ae
Karel Zimmer <karel.zimmer@gmail.com>
2024-04-30 10:28:56 +0200

    Refactor

--- usr/bin/kz-install-ubuntu-desktop.sh
+++ usr/bin/kz-install-ubuntu-desktop.sh
@@ -294,7 +294,7 @@ sudo apt-get remove --yes locate
 
 
 # Install APP monitors HOST pc06
-if [[ -f /home/${SUDO_USER:-$USER}/.config/monitors.xml ]]; then sudo cp  --preserve --verbose /home/"${SUDO_USER:-$USER}"/.config/monitors.xml ~gdm/.config/monitors.xml; fi
+if [[ -f /home/${SUDO_USER:-$USER}/.config/monitors.xml ]]; then sudo cp --preserve --verbose /home/"${SUDO_USER:-$USER}"/.config/monitors.xml ~gdm/.config/monitors.xml; fi
 if [[ -f ~gdm/.config/monitors.xml ]]; then sudo chown --verbose gdm:gdm ~gdm/.config/monitors.xml; fi
 
 # Remove APP monitors HOST pc06
