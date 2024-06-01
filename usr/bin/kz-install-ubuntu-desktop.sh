63776f69b643182046eb2d8d94a70a3296ce90aa
Karel Zimmer <karel.zimmer@gmail.com>
2024-06-01 17:58:52 +0200

    Install gnome-dependent packages only on all computers if gnome is used

--- usr/bin/kz-install-ubuntu-desktop.sh
+++ usr/bin/kz-install-ubuntu-desktop.sh
@@ -166,10 +166,10 @@ sudo apt-get install --yes gnome-gmail
 # Remove gnome-gmail from pc01 pc06
 sudo apt-get remove --yes gnome-gmail
 
-# Install gnome-tweaks on *
+# Install gnome-tweaks on pc01 pc06
 sudo apt-get install --yes gnome-tweaks
 
-# Remove gnome-tweaks from *
+# Remove gnome-tweaks from pc01 pc06
 sudo apt-get remove --yes gnome-tweaks
 
 # Install gnome-web on pc06
@@ -182,14 +182,16 @@ sudo apt-get remove --yes epiphany-browser
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] https://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list > /dev/null
 wget --output-document=- 'https://dl.google.com/linux/linux_signing_key.pub' | sudo gpg --dearmor --yes --output=/usr/share/keyrings/google-chrome.gpg
 sudo apt-get update
+sudo apt-get install --yes google-chrome-stable
 # Also install chrome-gnome-shell to make extensions.gnome.org work.
-sudo apt-get install --yes google-chrome-stable chrome-gnome-shell
+if type gnome-session &> /dev/null; then sudo apt-get install --yes chrome-gnome-shell; fi
 # Add the source list again because the installation overwrote the newly added source list.
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] https://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list > /dev/null
 # Remove the apt-key added during installation as an apt-key is no longer needed.
 sudo rm --force --verbose /etc/apt/trusted.gpg.d/google-chrome.gpg
 
 # Remove google-chrome from *
+if type gnome-session &> /dev/null; then apt-get remove --yes chrome-gnome-shell; fi
 sudo apt-get remove --yes google-chrome-stable chrome-gnome-shell
 sudo rm --force --verbose /etc/apt/sources.list.d/google-chrome.list* /usr/share/keyrings/google-chrome.gpg* /etc/apt/trusted.gpg.d/google-chrome.gpg
 sudo apt-get update
