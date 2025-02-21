af3e22b6b4ecd375e727712b892cd21e9f494ce0
Karel Zimmer <karel.zimmer@gmail.com>
2025-02-21 09:43:55 +0100

    Add check to GSettings scheme to allow error free installation on e.g. KDE

--- usr/bin/kz-setup.sh
+++ usr/bin/kz-setup.sh
@@ -49,16 +49,18 @@ if $GUI ; then rm --force --verbose ~/.local/share/applications/kz-cockpit.deskt
 # which has been replaced by gnome-shell-extension-ubuntu-dock.
 if $GUI && $APT && apt-cache show gnome-shell-extension-dashtodock &> /dev/null && ! [[ $(uname --kernel-version) =~ 'Ubuntu' ]] ; then gnome-extensions enable dash-to-dock@micxgx.gmail.com ; fi
 if $GUI && $APT && apt-cache show gnome-shell-extension-no-overview &> /dev/null ; then gsettings set org.gnome.shell.extensions.dash-to-dock disable-overview-on-startup true ; fi
+
 if $GUI && $RPM ; then gnome-extensions enable dash-to-dock@gnome-shell-extensions.gcampax.github.com ; fi
 if $GUI && $RPM ; then gnome-extensions enable no-overview@fthx ; fi
-if $GUI ; then gsettings set org.gnome.shell disable-user-extensions false ; fi
-if $GUI ; then gsettings set org.gnome.shell.extensions.dash-to-dock apply-custom-theme true ; fi
-if $GUI ; then gsettings set org.gnome.shell.extensions.dash-to-dock click-action minimize-or-previews ; fi
-if $GUI ; then gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32 ; fi
-if $GUI ; then gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed true ; fi
-if $GUI ; then gsettings set org.gnome.shell.extensions.dash-to-dock dock-position LEFT ; fi
-if $GUI ; then gsettings set org.gnome.shell.extensions.dash-to-dock extend-height true ; fi
-if $GUI ; then gsettings set org.gnome.shell.extensions.dash-to-dock icon-size-fixed true ; fi
+
+if $GUI && apt-cache show org.gnome.shell &> /dev/null ; then gsettings set org.gnome.shell disable-user-extensions false ; fi
+if $GUI && apt-cache show org.gnome.shell.extensions.dash-to-dock &> /dev/null ; then gsettings set org.gnome.shell.extensions.dash-to-dock apply-custom-theme true ; fi
+if $GUI && apt-cache show org.gnome.shell.extensions.dash-to-dock &> /dev/null ; then gsettings set org.gnome.shell.extensions.dash-to-dock click-action minimize-or-previews ; fi
+if $GUI && apt-cache show org.gnome.shell.extensions.dash-to-dock &> /dev/null ; then gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32 ; fi
+if $GUI && apt-cache show org.gnome.shell.extensions.dash-to-dock &> /dev/null ; then gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed true ; fi
+if $GUI && apt-cache show org.gnome.shell.extensions.dash-to-dock &> /dev/null ; then gsettings set org.gnome.shell.extensions.dash-to-dock dock-position LEFT ; fi
+if $GUI && apt-cache show org.gnome.shell.extensions.dash-to-dock &> /dev/null ; then gsettings set org.gnome.shell.extensions.dash-to-dock extend-height true ; fi
+if $GUI && apt-cache show org.gnome.shell.extensions.dash-to-dock &> /dev/null ; then gsettings set org.gnome.shell.extensions.dash-to-dock icon-size-fixed true ; fi
 
 # Reset dash-to-dock on *
 # Desktop dock.
@@ -67,16 +69,18 @@ if $GUI ; then gsettings set org.gnome.shell.extensions.dash-to-dock icon-size-f
 # which has been replaced by gnome-shell-extension-ubuntu-dock.
 if $GUI && $APT && apt-cache show gnome-shell-extension-dashtodock &> /dev/null && ! [[ $(uname --kernel-version) =~ 'Ubuntu' ]] ; then gnome-extensions disable dash-to-dock@micxgx.gmail.com ; fi
 if $GUI && $APT && apt-cache show gnome-shell-extension-no-overview &> /dev/null ; then gsettings reset org.gnome.shell.extensions.dash-to-dock disable-overview-on-startup  ; fi
+
 if $GUI && $RPM ; then gnome-extensions disable dash-to-dock@gnome-shell-extensions.gcampax.github.com ; fi
 if $GUI && $RPM ; then echo 'Goto https://extensions.gnome.org/extension/4099/no-overview/, install, and switch OFF' ; fi
-if $GUI ; then gsettings reset org.gnome.shell disable-user-extensions ; fi
-if $GUI ; then gsettings reset org.gnome.shell.extensions.dash-to-dock apply-custom-theme ; fi
-if $GUI ; then gsettings reset org.gnome.shell.extensions.dash-to-dock click-action ; fi
-if $GUI ; then gsettings reset org.gnome.shell.extensions.dash-to-dock dash-max-icon-size ; fi
-if $GUI ; then gsettings reset org.gnome.shell.extensions.dash-to-dock dock-fixed ; fi
-if $GUI ; then gsettings reset org.gnome.shell.extensions.dash-to-dock dock-position ; fi
-if $GUI ; then gsettings reset org.gnome.shell.extensions.dash-to-dock extend-height ; fi
-if $GUI ; then gsettings reset org.gnome.shell.extensions.dash-to-dock icon-size-fixed ; fi
+
+if $GUI && apt-cache show org.gnome.shell &> /dev/null ; then gsettings reset org.gnome.shell disable-user-extensions ; fi
+if $GUI && apt-cache show org.gnome.shell.extensions.dash-to-dock &> /dev/null ; then gsettings reset org.gnome.shell.extensions.dash-to-dock apply-custom-theme ; fi
+if $GUI && apt-cache show org.gnome.shell.extensions.dash-to-dock &> /dev/null ; then gsettings reset org.gnome.shell.extensions.dash-to-dock click-action ; fi
+if $GUI && apt-cache show org.gnome.shell.extensions.dash-to-dock &> /dev/null ; then gsettings reset org.gnome.shell.extensions.dash-to-dock dash-max-icon-size ; fi
+if $GUI && apt-cache show org.gnome.shell.extensions.dash-to-dock &> /dev/null ; then gsettings reset org.gnome.shell.extensions.dash-to-dock dock-fixed ; fi
+if $GUI && apt-cache show org.gnome.shell.extensions.dash-to-dock &> /dev/null ; then gsettings reset org.gnome.shell.extensions.dash-to-dock dock-position ; fi
+if $GUI && apt-cache show org.gnome.shell.extensions.dash-to-dock &> /dev/null ; then gsettings reset org.gnome.shell.extensions.dash-to-dock extend-height ; fi
+if $GUI && apt-cache show org.gnome.shell.extensions.dash-to-dock &> /dev/null ; then gsettings reset org.gnome.shell.extensions.dash-to-dock icon-size-fixed ; fi
 
 
 # Setup evolution on pc07
@@ -124,45 +128,45 @@ git config --global --unset alias.logg
 
 # Setup gnome on *
 # Desktop environment.
-if $GUI ; then gsettings set org.gnome.desktop.calendar show-weekdate true ; fi
-if $GUI ; then gsettings set org.gnome.desktop.interface clock-show-date true ; fi
-if $GUI ; then gsettings set org.gnome.desktop.interface clock-show-weekday true ; fi
-if $GUI ; then gsettings set org.gnome.desktop.interface show-battery-percentage true ; fi
-if $GUI ; then gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true ; fi
-if $GUI ; then gsettings set org.gnome.desktop.screensaver lock-enabled false ; fi
-if $GUI ; then gsettings set org.gnome.desktop.session idle-delay 900 ; fi
-if $GUI ; then gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true ; fi
-if $GUI ; then gsettings set org.gnome.desktop.wm.preferences button-layout :minimize,maximize,close ; fi
-if $GUI ; then gsettings set org.gnome.mutter center-new-windows true ; fi
-if $GUI ; then gsettings set org.gnome.nautilus.icon-view default-zoom-level large ; fi
-if $GUI ; then gsettings set org.gnome.nautilus.preferences click-policy single ; fi
-if $GUI ; then gsettings set org.gnome.nautilus.preferences open-folder-on-dnd-hover true ; fi
-if $GUI ; then gsettings set org.gnome.nautilus.preferences show-create-link true ; fi
-if $GUI ; then gsettings set org.gnome.nautilus.preferences show-image-thumbnails always ; fi
-if $GUI ; then gsettings set org.gnome.settings-daemon.plugins.power power-button-action interactive ; fi
-if $GUI ; then gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type nothing ; fi
-if $GUI ; then gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type nothing ; fi
+if $GUI && apt-cache show org.gnome.desktop.calendar &> /dev/null ; then gsettings set org.gnome.desktop.calendar show-weekdate true ; fi
+if $GUI && apt-cache show org.gnome.desktop.interface &> /dev/null ; then gsettings set org.gnome.desktop.interface clock-show-date true ; fi
+if $GUI && apt-cache show org.gnome.desktop.interface &> /dev/null ; then gsettings set org.gnome.desktop.interface clock-show-weekday true ; fi
+if $GUI && apt-cache show org.gnome.desktop.interface &> /dev/null ; then gsettings set org.gnome.desktop.interface show-battery-percentage true ; fi
+if $GUI && apt-cache show org.gnome.desktop.peripherals.touchpad &> /dev/null ; then gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true ; fi
+if $GUI && apt-cache show org.gnome.desktop.screensaver &> /dev/null ; then gsettings set org.gnome.desktop.screensaver lock-enabled false ; fi
+if $GUI && apt-cache show org.gnome.desktop.session &> /dev/null ; then gsettings set org.gnome.desktop.session idle-delay 900 ; fi
+if $GUI && apt-cache show org.gnome.desktop.sound &> /dev/null ; then gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true ; fi
+if $GUI && apt-cache show org.gnome.desktop.wm.preferences &> /dev/null ; then gsettings set org.gnome.desktop.wm.preferences button-layout :minimize,maximize,close ; fi
+if $GUI && apt-cache show org.gnome.mutter &> /dev/null ; then gsettings set org.gnome.mutter center-new-windows true ; fi
+if $GUI && apt-cache show org.gnome.nautilus.icon-view &> /dev/null ; then gsettings set org.gnome.nautilus.icon-view default-zoom-level large ; fi
+if $GUI && apt-cache show org.gnome.nautilus.preferences &> /dev/null ; then gsettings set org.gnome.nautilus.preferences click-policy single ; fi
+if $GUI && apt-cache show org.gnome.nautilus.preferences &> /dev/null ; then gsettings set org.gnome.nautilus.preferences open-folder-on-dnd-hover true ; fi
+if $GUI && apt-cache show org.gnome.nautilus.preferences &> /dev/null ; then gsettings set org.gnome.nautilus.preferences show-create-link true ; fi
+if $GUI && apt-cache show org.gnome.nautilus.preferences &> /dev/null ; then gsettings set org.gnome.nautilus.preferences show-image-thumbnails always ; fi
+if $GUI && apt-cache show org.gnome.settings-daemon.plugins.power &> /dev/null ; then gsettings set org.gnome.settings-daemon.plugins.power power-button-action interactive ; fi
+if $GUI && apt-cache show org.gnome.settings-daemon.plugins.power &> /dev/null ; then gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type nothing ; fi
+if $GUI && apt-cache show org.gnome.settings-daemon.plugins.power &> /dev/null ; then gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type nothing ; fi
 
 # Reset gnome on *
 # Desktop environment.
-if $GUI ; then gsettings reset org.gnome.desktop.calendar show-weekdate ; fi
-if $GUI ; then gsettings reset org.gnome.desktop.interface clock-show-date ; fi
-if $GUI ; then gsettings reset org.gnome.desktop.interface clock-show-weekday ; fi
-if $GUI ; then gsettings reset org.gnome.desktop.interface show-battery-percentage ; fi
-if $GUI ; then gsettings reset org.gnome.desktop.peripherals.touchpad tap-to-click ; fi
-if $GUI ; then gsettings reset org.gnome.desktop.screensaver lock-enabled ; fi
-if $GUI ; then gsettings reset org.gnome.desktop.session idle-delay ; fi
-if $GUI ; then gsettings reset org.gnome.desktop.sound allow-volume-above-100-percent ; fi
-if $GUI ; then gsettings reset org.gnome.desktop.wm.preferences button-layout ; fi
-if $GUI ; then gsettings reset org.gnome.mutter center-new-windows ; fi
-if $GUI ; then gsettings reset org.gnome.nautilus.icon-view default-zoom-level ; fi
-if $GUI ; then gsettings reset org.gnome.nautilus.preferences click-policy ; fi
-if $GUI ; then gsettings reset org.gnome.nautilus.preferences open-folder-on-dnd-hover ; fi
-if $GUI ; then gsettings reset org.gnome.nautilus.preferences show-create-link ; fi
-if $GUI ; then gsettings reset org.gnome.nautilus.preferences show-image-thumbnails ; fi
-if $GUI ; then gsettings reset org.gnome.settings-daemon.plugins.power power-button-action ; fi
-if $GUI ; then gsettings reset org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type ; fi
-if $GUI ; then gsettings reset org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type ; fi
+if $GUI && apt-cache show org.gnome.desktop.calendar &> /dev/null ; then gsettings reset org.gnome.desktop.calendar show-weekdate ; fi
+if $GUI && apt-cache show org.gnome.desktop.interface &> /dev/null ; then gsettings reset org.gnome.desktop.interface clock-show-date ; fi
+if $GUI && apt-cache show org.gnome.desktop.interface &> /dev/null ; then gsettings reset org.gnome.desktop.interface clock-show-weekday ; fi
+if $GUI && apt-cache show org.gnome.desktop.interface &> /dev/null ; then gsettings reset org.gnome.desktop.interface show-battery-percentage ; fi
+if $GUI && apt-cache show org.gnome.desktop.peripherals.touchpad &> /dev/null ; then gsettings reset org.gnome.desktop.peripherals.touchpad tap-to-click ; fi
+if $GUI && apt-cache show org.gnome.desktop.screensaver &> /dev/null ; then gsettings reset org.gnome.desktop.screensaver lock-enabled ; fi
+if $GUI && apt-cache show org.gnome.desktop.session &> /dev/null ; then gsettings reset org.gnome.desktop.session idle-delay ; fi
+if $GUI && apt-cache show org.gnome.desktop.sound &> /dev/null ; then gsettings reset org.gnome.desktop.sound allow-volume-above-100-percent ; fi
+if $GUI && apt-cache show org.gnome.desktop.wm.preferences &> /dev/null ; then gsettings reset org.gnome.desktop.wm.preferences button-layout ; fi
+if $GUI && apt-cache show org.gnome.mutter &> /dev/null ; then gsettings reset org.gnome.mutter center-new-windows ; fi
+if $GUI && apt-cache show org.gnome.nautilus.icon-view &> /dev/null ; then gsettings reset org.gnome.nautilus.icon-view default-zoom-level ; fi
+if $GUI && apt-cache show org.gnome.nautilus.preferences &> /dev/null ; then gsettings reset org.gnome.nautilus.preferences click-policy ; fi
+if $GUI && apt-cache show org.gnome.nautilus.preferences &> /dev/null ; then gsettings reset org.gnome.nautilus.preferences open-folder-on-dnd-hover ; fi
+if $GUI && apt-cache show org.gnome.nautilus.preferences &> /dev/null ; then gsettings reset org.gnome.nautilus.preferences show-create-link ; fi
+if $GUI && apt-cache show org.gnome.nautilus.preferences &> /dev/null ; then gsettings reset org.gnome.nautilus.preferences show-image-thumbnails ; fi
+if $GUI && apt-cache show org.gnome.settings-daemon.plugins.power &> /dev/null ; then gsettings reset org.gnome.settings-daemon.plugins.power power-button-action ; fi
+if $GUI && apt-cache show org.gnome.settings-daemon.plugins.power &> /dev/null ; then gsettings reset org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type ; fi
+if $GUI && apt-cache show org.gnome.settings-daemon.plugins.power &> /dev/null ; then gsettings reset org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type ; fi
 
 
 # Setup google-chrome on *
