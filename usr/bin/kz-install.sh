3af5229d9a21aa69f96e4d7c165eb709991dd354
Karel Zimmer <karel.zimmer@gmail.com>
2025-02-24 10:06:41 +0100

    Add snap refresh for RPM system

--- usr/bin/kz-install.sh
+++ usr/bin/kz-install.sh
@@ -27,14 +27,7 @@ if systemctl cat apport &> /dev/null ; then sudo systemctl enable --now apport.s
 # Install update-system on *
 # Update system.
 # This may take a while...
-if $APT ; then sudo apt-get update ; fi
-if $APT ; then sudo apt-get upgrade --assume-yes ; fi
-if $APT && type snap &> /dev/null ; then sudo snap refresh ; fi
-
-if $RPM ; then sudo dnf check-update --refresh || true ; fi
-if $RPM ; then sudo dnf upgrade --assumeyes --refresh ; fi
-if $RPM && type snap &> /dev/null ; then sudo snap refresh ; fi
-
+kz-update
 
 # Remove update-system from *
 # Update system.
