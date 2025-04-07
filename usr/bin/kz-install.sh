6c5a80e04e2b6ebcfbcf38c184d7751f888e607c
Karel Zimmer <karel.zimmer@gmail.com>
2025-04-07 08:12:15 +0200

    Add man section 7

--- usr/bin/kz-install.sh
+++ usr/bin/kz-install.sh
@@ -5,7 +5,7 @@
 # SPDX-FileCopyrightText: Karel Zimmer <info@karelzimmer.nl>
 # SPDX-License-Identifier: CC0-1.0
 ###############################################################################
-# Use "man kz-install.sh" to learn more about the format of this file.
+# Use "man 7 kz-install.sh" to learn more about the format of this file.
 
 # Install 1-disabled-apport on *
 # Disable Ubuntu's automatic crash report generation.
