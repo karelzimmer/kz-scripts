1a30294421a61d368b88edc4abebdea107f4a8ca
Karel Zimmer <karel.zimmer@gmail.com>
2025-07-09 21:25:50 +0200

    Refactor code structure for improved readability and maintainability

--- usr/bin/kz_common.sh
+++ usr/bin/kz_common.sh
@@ -65,8 +65,7 @@ function kz.check_debian_package_manager() {
     fi
 
     while pkexec /usr/bin/kz_common-pkexec; do
-        text=$(eval_gettext "Wait \$sleep second for another package manager \
-to finish")
+        text=$(gettext "Wait 1 second for another package manager to finish")
         if ${OPTION_GUI:-false}; then
             kz.logmsg "$text..."
             # Inform the user in 'zenity --progress' why there is a wait.
