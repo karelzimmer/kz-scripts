beef05ef533397335dc32bad0f86acaedaf49156
Karel Zimmer <karel.zimmer@gmail.com>
2025-08-22 11:56:08 +0200

    Add cleanup of temporary files on script exit

--- usr/bin/kz_common.sh
+++ usr/bin/kz_common.sh
@@ -361,6 +361,12 @@ $(eval_gettext "Program \$PROGRAM_NAME encountered an error.")"
             exit "$rc"
             ;;
         exit )
+            # Clean up temporary files.
+            text='Cleaning up temporary files...'
+            kz.logmsg "$text"
+            rm  --verbose                           \
+                --force                             \
+                "/tmp/$PROGRAM_NAME"-??????????*    |& $PROGRAM_LOGS
             text="Ended (code=exited, status=$status)."
             kz.logmsg "$text"
             text="==== END logs for script $PROGRAM_NAME ====================="
