137b663649eac10bde1745894270ac6fde7fa78b
Karel Zimmer <karel.zimmer@gmail.com>
2025-11-10 12:34:14 +0100

    fix: standardize spacing in usage comments for fdupes and ntfsfix in kz-install.sh

--- usr/bin/kz-install.sh
+++ usr/bin/kz-install.sh
@@ -790,7 +790,7 @@ if grep --quiet rhel   /etc/os-release; then sudo dnf     install --assumeyes  n
 # $ findmnt # or lsblk
 # TARGET SOURCE FSTYPE OPTIONS
 # /media/... /dev/sda1 ntfs3 rw,nosuid,nodev,relatime,uid=...
-# 
+#
 # $ lsblk   # or findmnt
 # NAME MAJ:MIN RM SIZE RO TYPE MOUNTPOINTS
 # sda 8:0 0 931,5G 0 disk
