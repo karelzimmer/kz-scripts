4d8c1181edebcb7f4f027b989c2249a4e772dc3b
karelzimmer <karel.zimmer@gmail.com>
2026-01-17 18:56:52 +0100

    feat: add needrestart installation and removal for improved library management

--- usr/bin/kz-install.sh
+++ usr/bin/kz-install.sh
@@ -751,6 +751,20 @@ if grep --quiet --regexp=debian /etc/os-release; then sudo rm  --force --verbose
 if grep --quiet --regexp=debian /etc/os-release; then sudo apt-get update; fi
 if grep --quiet --regexp=rhel   /etc/os-release; then sudo flatpak uninstall --assumeyes  flathub com.microsoft.Edge; fi
 
+# INSTALL needrestart pc06 pc07
+# -----------------------------------------------------------------------------
+# Check which daemons need to be restarted after library upgrades.
+# -----------------------------------------------------------------------------
+if grep --quiet --regexp=debian /etc/os-release; then sudo apt-get install --assume-yes needrestart; fi
+if grep --quiet --regexp=rhel   /etc/os-release; then sudo dnf     install --assumeyes  needrestart; fi
+
+# REMOVE needrestart pc06 pc07
+# -----------------------------------------------------------------------------
+# Check which daemons need to be restarted after library upgrades.
+# -----------------------------------------------------------------------------
+if grep --quiet --regexp=debian /etc/os-release; then sudo apt-get remove --assume-yes needrestart; fi
+if grep --quiet --regexp=rhel   /etc/os-release; then sudo dnf     remove --assumeyes  needrestart; fi
+
 # INSTALL nmap pc06 pc07
 # -----------------------------------------------------------------------------
 # Network MAPper.
