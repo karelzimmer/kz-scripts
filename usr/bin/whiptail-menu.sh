afee83e0ce142ae89da57aa738f2c89df40603ab
karelzimmer <karel.zimmer@gmail.com>
2026-04-06 21:08:44 +0200

    feature: TUIfy scripts

--- usr/bin/whiptail-menu.sh
+++ usr/bin/whiptail-menu.sh
@@ -32,18 +32,72 @@ readonly -a  MENU1_DESCS=(
                 "$(gettext 'Show command menu')"
                 "$(gettext 'Exit menu')"
             )
+
+readonly -a  MENU2_ACTIONS=(
+            [1]="$(gettext 'Make backup')"
+                "$(gettext 'Show WiFi details')"
+                "$(gettext 'Install apps')"
+                "$(gettext 'Restore backup')"
+                "$(gettext 'Set up apps')"
+                "$(gettext 'Go to installation menu')"
+            )
+readonly -a  MENU2_DESCS=(
+            [1]='kz backup'
+                'kz wifi'
+                'kz install'
+                'kz restore'
+                'kz setup'
+                "$(gettext 'Back')"
+            )
+
 title=$(gettext 'Installation menu')
 text=$(gettext 'Select a choice:')
-# --menu <text> <height> <width> <listheight> [tag item] ...
+
 REPLY=$(
-    whiptail    --title "$title"                            \
-                --menu  "$text"                             \
-                25 60 10                                    \
-                1 "$(printf '%-25s' "${MENU1_ACTIONS[1]}" "${MENU1_DESCS[1]}")"   \
-                2 "$(printf '%-25s' "${MENU1_ACTIONS[2]}" "${MENU1_DESCS[2]}")"   \
-                3 "$(printf '%-25s' "${MENU1_ACTIONS[3]}" "${MENU1_DESCS[3]}")"   \
-                4 "$(printf '%-25s' "${MENU1_ACTIONS[4]}" "${MENU1_DESCS[4]}")"   \
-                5 "$(printf '%-25s' "${MENU1_ACTIONS[5]}" "${MENU1_DESCS[5]}")"   \
+    whiptail    --title "$title"        \
+                --menu  "$text"         \
+                25 60 10                \
+                1 " $(printf '%-26s'    \
+                "${MENU1_ACTIONS[1]}"   \
+                "${MENU1_DESCS[1]}")"   \
+                2 " $(printf '%-26s'    \
+                "${MENU1_ACTIONS[2]}"   \
+                "${MENU1_DESCS[2]}")"   \
+                3 " $(printf '%-26s'    \
+                "${MENU1_ACTIONS[3]}"   \
+                "${MENU1_DESCS[3]}")"   \
+                4 " $(printf '%-26s'    \
+                "${MENU1_ACTIONS[4]}"   \
+                "${MENU1_DESCS[4]}")"   \
+                5 " $(printf '%-26s'    \
+                "${MENU1_ACTIONS[5]}"   \
+                "${MENU1_DESCS[5]}")"   \
+                2>&1 >/dev/tty 
+    )
+echo "You selected: $REPLY"
+
+title=$(gettext 'Command menu')
+text=$(gettext 'Select a choice:')
+
+REPLY=$(
+    whiptail    --title "$title"        \
+                --menu  "$text"         \
+                25 60 10                \
+                1 " $(printf '%-26s'    \
+                "${MENU2_ACTIONS[1]}"   \
+                "${MENU2_DESCS[1]}")"   \
+                2 " $(printf '%-26s'    \
+                "${MENU2_ACTIONS[2]}"   \
+                "${MENU2_DESCS[2]}")"   \
+                3 " $(printf '%-26s'    \
+                "${MENU2_ACTIONS[3]}"   \
+                "${MENU2_DESCS[3]}")"   \
+                4 " $(printf '%-26s'    \
+                "${MENU2_ACTIONS[4]}"   \
+                "${MENU2_DESCS[4]}")"   \
+                5 " $(printf '%-26s'    \
+                "${MENU2_ACTIONS[5]}"   \
+                "${MENU2_DESCS[5]}")"   \
                 2>&1 >/dev/tty 
     )
 echo "You selected: $REPLY"
