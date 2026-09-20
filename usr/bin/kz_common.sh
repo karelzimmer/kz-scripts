504ec67e526819ec420cafe13733dd80ad59378e
Karel Zimmer <karel.zimmer@gmail.com>
2026-09-20 21:02:41 +0200

    Refactor error handling in kz_common.sh

--- usr/bin/kz_common.sh
+++ usr/bin/kz_common.sh
@@ -226,135 +226,74 @@ kz.term() {
     local command=$4
     local -i rc=$5
 
-    local -i rc_desc_signalno=0
-    local bold='\033[1m'
-    local rc_desc=''
-    local status=$rc/FAILURE
-    local text=''
-
-    case $rc in
-        0 )
-            rc_desc='successful termination'
-            status=0/SUCCESS
-            ;;
-        1 )
-            rc_desc='terminated with error'
-            ;;
-        6[4-9] | 7[0-8] )                   # 64--78
-            rc_desc="open file '/usr/include/sysexits.h' and look for '$rc'"
-            ;;
-        100 )
-            if grep --quiet --regexp='debian' /etc/os-release; then
-                rc_desc='apt/dpkg exited with error'
-            elif grep --quiet --regexp='rhel\|fedora' /etc/os-release; then
-                rc_desc='there are updates available'
-            else
-                rc_desc="previous errors/it didn't work"
-            fi
-            ;;
-        126 )
-            rc_desc='command cannot execute'
-            ;;
-        127 )
-            rc_desc='command not found'
-            ;;
-        128 )
-            rc_desc='invalid argument to exit'
-            ;;
-        129 )                               # SIGHUP (128 + 1)
-            rc_desc='hangup'
-            ;;
-        130 )                               # SIGINT (128 + 2)
-            rc_desc='terminated by control-c'
-            ;;
-        13[1-9] | 140 )                     # 131 (128 + 3)--140 (128 + 12)
-            rc_desc_signalno=$(( rc - 128 ))
-            rc_desc="typ 'trap -l' and look for $rc_desc_signalno"
-            ;;
-        141 )                               # SIGPIPE (128 + 13)
-            rc_desc='broken pipe: write to pipe with no readers'
-            ;;
-        142 )                               # SIGALRM (128 + 14)
-            rc_desc='timer signal from alarm'
-            ;;
-        143 )                               # SIGTERM (128 + 15)
-            rc_desc='termination signal'
-            ;;
-        14[4-9] | 1[5-8][0-9] | 19[0-2])    # 144 (128 + 16)--192 (128 + 64)
-            rc_desc_signalno=$(( rc - 128 ))
-            rc_desc="typ 'trap -l' and look for $rc_desc_signalno"
-            ;;
-        200 )
-            # Red Hat or Red Hat-based system.
-            rc_desc='There was a problem with acquiring or releasing of locks.'
-            ;;
-        255 )
-            rc_desc='exit status out of range'
-            ;;
-        * )
-            rc_desc='unknown error'
-            ;;
-    esac
-
-    text="Signal: $signal, line: $lineno, function: $function, "
-    text+="command: $command, exit code: $rc ($rc_desc)."
+    text="Signal   : $signal\n"
+    text+="Line     : $lineno\n"
+    text+="Function : $function\n"
+    text+="Command  : $command\n"
+    text+="Exit code: $rc"
     kz.logmsg "$text"
 
     case $signal in
         err )
-            text="$(eval_gettext \
-"Program \$PROGRAM_NAME encountered an error.")"
-            kz.errmsg "$text"
-            text="$(eval_gettext \
-"Error \$rc on line \$lineno in function \$function while executing command: \
-\$command")"
-            if [[ -z ${XDG_CURRENT_DESKTOP-} ]]; then
-                text+="
-$(gettext "Use \"journalctl -xe\" to check what went wrong.")"
-            else
-                text+="
-$(gettext "Use \"journalctl -xe\" to check what went wrong.")
-$(gettext "The last few lines of the log are displayed here.")
-                $(
-                    journalctl  --all                           \
-                                --catalog                       \
-                                --lines                         \
-                                --no-pager                      \
-                                --pager-end                     \
-                                --identifier="$PROGRAM_NAME"
-                )"
-            fi
-            kz.errmsg "$text"
-            exit "$rc"
+            kz.term_err
             ;;
         exit )
-            if [[ ${UI_MODE-} = 'tui' ]]; then
-                reset
-                clear -x
-            fi
-
-            if [[ $rc -eq 0 ]]; then
-                text='Cleaning up temporary files...'
-                kz.logmsg "$text"
-                rm --force --verbose /tmp/"$PROGRAM_NAME"-* |& $PROGRAM_LOGS ||
-                    true
-            fi
-            text="Ended (code=exited, status=$status)."
-            kz.logmsg "$text"
-            text="${bold}END logging for script $PROGRAM_NAME${NORMAL}"
-            kz.logmsg "$text"
-            trap - ERR EXIT SIGHUP SIGINT SIGTERM
-            exit "$rc"
+            kz.term_exit
             ;;
         * )
-            if [[ ${UI_MODE-} = 'tui' ]]; then
-                reset
-                clear -x
-            fi
-            text="$(eval_gettext \
-"Program \$PROGRAM_NAME has been interrupted.")"
-            kz.errmsg "$text"
-            exit "$rc"
+            kz.term_int
             ;;
     esac
+}
+
+
+# This function processes unexpected command errors in scripts.
+kz.term_err() {
+    local text=''
+
+    text=$(eval_gettext "Program \$PROGRAM_NAME encountered an error.")
+    if [[ ${UI_MODE-} != 'gui' ]]; then
+        text+="\n"
+        text+=$(gettext "Use \"journalctl -xe\" to check what went wrong.")
+    fi
+    kz.errmsg "$text"
+
+    exit "$rc"
+}
+
+
+# This function processes scripts exits.
+kz.term_exit() {
+    local bold='\033[1m'
+    local text=''
+
+    if [[ ${UI_MODE-} = 'tui' ]]; then
+        reset
+        clear -x
+    fi
+    if [[ $rc -eq 0 ]]; then
+        text='Cleaning up temporary files...'
+        kz.logmsg "$text"
+        rm  --force                 \
+            --verbose               \
+            /tmp/"$PROGRAM_NAME"-*  |& $PROGRAM_LOGS || true
+    fi
+    text="${bold}END logging for script $PROGRAM_NAME${NORMAL}"
+    kz.logmsg "$text"
+    trap - ERR EXIT SIGHUP SIGINT SIGTERM
+    exit "$rc"
+}
+
+
+# This function processes standard interruption signals.
+kz.term_int() {
+    local text=''
+
+    if [[ ${UI_MODE-} = 'tui' ]]; then
+        reset
+        clear -x
+    fi
+    text="$(eval_gettext "Program \$PROGRAM_NAME has been interrupted.")"
+    kz.errmsg "$text"
+    exit "$rc"
 }
